b4fb2b51a4ff775f8f75746157acd88267542d87
BrayanSGL <bsgaleanol@udistrital.edu.co>
2024-08-24 11:34:38 -0500

    chore: Agregar Dockerfile y archivos iniciales para el proyecto de Node.js II

---
+++ node_js_II_3/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const path = require("path");
+
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+app.get("/db", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/db.html"));
+});
+app.get("/inicio", (req, res) => {
+  res.sendFile(path.join(__dirname, "/index.html"));
+});
+
+app.post("/", (req, res) => {
+  var mensaje = req.body.msg;
+  res.send("Mensaje recibido: " + mensaje);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
